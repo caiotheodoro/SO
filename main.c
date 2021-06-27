45091230629fe2393adfad9b11cc8ea2f3531dfe
caiotheodoro <caio.2001@alunos.utfpr.edu.br>
2021-06-27 20:51:17 -0300

    commit3

--- main.c
+++ main.c
@@ -1,21 +1,26 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[]){
     if(argc < 2) return printf("ERRO: Precisa especificar o nivel da arvore.\n");
- 
+    
     int N = atoi(argv[1]);
     int arvore = 0;
- 
+    int arvPai[N];
+
     for(int i = 0; i < N; i++){
+        int pai = getpid();
         int pid = fork();
-        if(!pid) arvore++;        
+        if(!pid){
+            arvPai[arvore] = pai;
+            arvore++;
+        }      
     }
- 
-    for(int i = 0; i < arvore; i++) printf("-\t");
-    printf("PID: %d\n", getpid());
- 
+    
+    for(int i = 0; i < arvore; i++) printf("PaiID %d -\t", arvPai[i]);
+    printf("((PID: %d))\n", getpid());
+
     int x;
     scanf("%d", &x);
     return 0;
