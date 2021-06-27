f257555394d4767cc453b409d4e27dc6b47e34ac
caiotheodoro <caio.2001@alunos.utfpr.edu.br>
2021-06-27 20:36:55 -0300

    commit2

--- main.c
+++ main.c
@@ -1,60 +1,22 @@
 #include <unistd.h>
 #include <stdio.h>
-
-
-int main(){
-
-    int i=0;
-    int N = 2;
-    int x;
-    while(i< N){
-    pid_t pid = fork();
-  
-    // If fork() returns zero then it
-    // means it is child process.
-    if (pid == 0) {
-  
-        // First child needs to be printed
-        // later hence this process is made
-        // to sleep for 3 seconds.
-        sleep(3);
-  
-        // This is first child process
-        // getpid() gives the process
-        // id and getppid() gives the
-        // parent id of that process.
-     //   printf("child[1] --> pid = %d and ppid = %d\n",
-      //         getpid(), getppid());
-    }
-  
-    else {
-        pid_t pid1 = fork();
-        if (pid1 == 0) {
-            sleep(2);
-          //  printf("child[2] --> pid = %d and ppid = %d\n",
-           //        getpid(), getppid());
-        }
-        else {
-
-            // If value returned from fork()
-            // in not zero and >0 that means
-            // this is parent process.
+#include <stdlib.h>
  
-            // This is asked to be printed at last
-            // hence made to sleep for 3 seconds.
-            sleep(3);
-           // printf("parent --> pid = %d\n", getpid());
-            }
-        }        
-        i++;
-        for(int j=0;j<i;j++){
-            printf("-");
-        }
-         printf(" PID %d: %d \n",i, getpid());
+int main(int argc, char *argv[]){
+    if(argc < 2) return printf("ERRO: Precisa especificar o nivel da arvore.\n");
+ 
+    int N = atoi(argv[1]);
+    int arvore = 0;
+ 
+    for(int i = 0; i < N; i++){
+        int pid = fork();
+        if(!pid) arvore++;        
     }
-
-    scanf("%d",&x);
-   
-
+ 
+    for(int i = 0; i < arvore; i++) printf("-\t");
+    printf("PID: %d\n", getpid());
+ 
+    int x;
+    scanf("%d", &x);
     return 0;
 }
