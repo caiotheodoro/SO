721e1394d1e8c63e6d2a47cd838c8fcd8b148e94
caiotheodoro <caio.2001@alunos.utfpr.edu.br>
2021-07-08 19:42:06 -0300

    documentacao ex1 e ex2

--- LAB3/ex1/main.c
+++ LAB3/ex1/main.c
@@ -11,10 +11,10 @@
 // #include "thread.h"
 
 typedef struct Data {
-    int pos_i, pos_f;
-    int *array;
-    int chave;
-    int seq;
+    int pos_i, pos_f; //demilitadores de posição para busca
+    int *array; // array para buscar
+    int chave; //valor a ser buscado               //scruct da thread
+    int seq; // numero da thread (0,1,2..)
 }Data;
 
 
@@ -26,14 +26,14 @@ Data* criaData(int ini, int fim, int* array, int chave, int seq){
   novo->chave = chave;
   novo->seq = seq;
   return novo;
-}
+}// inicializa a struct com os params passados
 
 
 void * new_thread(void* param){
-  struct Data* thread = param;
-  for(int i = thread->pos_i; i < thread->pos_f; i++){
-    if(thread->array[i] == thread->chave){
-      printf("Thread %d (ID %ld): Encontrado valor %d no index %d do vetor.\n", thread->seq, pthread_self(), thread->chave, i);
+  struct Data* thread = param; // struct na posição i
+  for(int i = thread->pos_i; i < thread->pos_f; i++){// percorre o array entre os delimitadores
+    if(thread->array[i] == thread->chave){ // verifica sequencialmente se é chave
+      printf("Thread %d (ID %ld): Encontrado valor %d no index %d do vetor.\n", thread->seq, pthread_self(), thread->chave, i);// printa se achou
     }
   }
 }
@@ -43,25 +43,25 @@ int main(int argc, char *argv[])
 {
   if(argc < 4) return printf("./$ <N_Threads> <valorBusca>");
 
-  int N = atoi(argv[1]);
-  int valorBuscar = atoi(argv[2]);
+  int N = atoi(argv[1]); // valor 1  do cmd, numero de thread
+  int valorBuscar = atoi(argv[2]); // valor 2 do cmd, numero para buscar
 
-  int tamVetor = N*50000;
-  rv* vtr = rv_gerar(tamVetor, 25000);
+  int tamVetor = N*50000; // tamanho do vetor (num thread * 50000)
+  rv* vtr = rv_gerar(tamVetor, 25000); //gera o vetor aleatorio com modulo 25000
 
-  pthread_t t1[N];
+  pthread_t t1[N]; //inicializa os indicadores da thread
 
 
   for(int i=0;i<N;i++){
 
-    int ini = i*(tamVetor/N);
-    int fim = (i+1)*(tamVetor/N)-1;
-    if((i+1) >= N) fim = tamVetor;
+    int ini = i*(tamVetor/N); //divide a fatia inicia de busca
+    int fim = (i+1)*(tamVetor/N)-1;//divide a fatia final de busca
+    if((i+1) >= N) fim = tamVetor; //verifica o ultimo 
 
-    Data* data_chunk = criaData(ini, fim, vtr->array, valorBuscar, i);
+    Data* data_chunk = criaData(ini, fim, vtr->array, valorBuscar, i);// inicializa struct
       //printf("v %d, ini %d, fim %d, 1 %d, 2 %d, 3 %d\n", data_chunk[i]->chave, data_chunk[i]->pos_i, data_chunk[i]->pos_f,data_chunk[i]->array[1], data_chunk[i]->array[2], data_chunk[i]->array[3]);
     
-    pthread_create(&t1[i], NULL, new_thread,  (void*) data_chunk);
+    pthread_create(&t1[i], NULL, new_thread,  (void*) data_chunk);// cria a thread
     // pthread_join (t1[i], NULL);
 
   }
--- LAB3/ex2/main.c
+++ LAB3/ex2/main.c
@@ -9,12 +9,12 @@
 #include <stdlib.h>
 #include "random_vector.h"
 
-int frequencia[2][10];
+int frequencia[2][10]; //vetor das frequencias
 
 typedef struct Data {
-    int pos_i, pos_f;
-    int *array;
-    int seq;
+    int pos_i, pos_f; //demilitadores de posição para busca
+    int *array; // array para buscar
+    int seq;// numero da thread (0,1,2..)
 }Data;
 
 
@@ -25,15 +25,15 @@ Data* criaData(int ini, int fim, int* array, int seq){
   novo->array = array;
   novo->seq = seq;
   return novo;
-}
+}// inicializa a struct com os params passados
 
 
 void* new_thread(void* param){
-  struct Data* thread = param;
+  struct Data* thread = param;// struct na posição i
 
-  for(int i = thread->pos_i; i < thread->pos_f; i++){
-    if(thread->array[i] >= 0 && thread->array[i] <= 9){
-      frequencia[thread->seq][thread->array[i]]++;
+  for(int i = thread->pos_i; i < thread->pos_f; i++){// percorre o array entre os delimitadores
+    if(thread->array[i] >= 0 && thread->array[i] <= 9){ //verificador
+      frequencia[thread->seq][thread->array[i]]++; //soma a frequẽncia
     }
   }
 
@@ -44,37 +44,37 @@ int main()
 {
   int tamVetor = 100000;
   int N = 2;
-  rv* vtr = rv_gerar(tamVetor, 10);
-  pthread_t t1[2];
+  rv* vtr = rv_gerar(tamVetor, 10); //gera o vetor com mod 10
+  pthread_t t1[2]; //identificadores de duas threads
 
   // zera todas frequencias
   for(int i = 0; i<2; i++){
     for(int j = 0; j<10; j++){
-      frequencia[i][j] = 0;
+      frequencia[i][j] = 0; //inicializa o vetor zerando as posições
     }
   }
 
   // obtem os dados
   for(int i=0;i<N;i++){
 
-    int ini = i*(tamVetor/N);
-    int fim = (i+1)*(tamVetor/N)-1;
-    if((i+1) >= N) fim = tamVetor;
+    int ini = i*(tamVetor/N);//divide a fatia inicia de busca
+    int fim = (i+1)*(tamVetor/N)-1;//divide a fatia final de busca
+    if((i+1) >= N) fim = tamVetor;//verifica o ultimo 
 
-    Data* data_chunk = criaData(ini, fim, vtr->array, i);
+    Data* data_chunk = criaData(ini, fim, vtr->array, i);// inicializa struct
     // printf("v %d, ini %d, fim %d, 1 %d, 2 %d, 3 %d\n", data_chunk->seq, data_chunk->pos_i, data_chunk->pos_f,data_chunk->array[1], data_chunk->array[2], data_chunk->array[3]);
     
-    pthread_create(&t1[i], NULL, new_thread,  (void*) data_chunk);
+    pthread_create(&t1[i], NULL, new_thread,  (void*) data_chunk);//inicializa thread
     // pthread_join (t1[i], NULL);
 
   }
 
-  pthread_join (t1[0], NULL);
-  pthread_join (t1[1], NULL);
+  pthread_join (t1[0], NULL);//sincroniza as threas
+  pthread_join (t1[1], NULL);//sincroniza as threas
 
   // calcula e printa as frequências
   for(int i=0; i<10; i++){
-    int freq = frequencia[0][i]+frequencia[1][i];
+    int freq = frequencia[0][i]+frequencia[1][i];//imprime frequências
     printf("Total de '%d' no vetor: %d \t-> Frequencia: %.2f%% \t-> (t1 %d + t2 %d)\n", i, freq, (float)(freq*100)/tamVetor, frequencia[0][i], frequencia[1][i]);
   }
 
