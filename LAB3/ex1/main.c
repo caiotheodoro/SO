b5c691bf0076346e8993e7d72d2ff7e66e0e636c
caiotheodoro <caio.2001@alunos.utfpr.edu.br>
2021-07-07 23:15:59 -0300

    ex1

--- LAB3/ex1/main.c
+++ LAB3/ex1/main.c
@@ -5,7 +5,32 @@
 #include "random_vector.h"
 #include "thread.h"
 
+typedef struct Data {
+    int pos_i,pos_f;
+    int *v1;
+    int chave;
+}Data;
 
+
+Data* criaData(int ini, int fim,int*v1, int chave){
+  Data* novo = (Data*) malloc (sizeof(Data));
+  novo->pos_i = ini;
+  novo->pos_f = fim;
+  novo->v1 = v1;
+  novo->chave = chave;
+
+  return novo;
+}
+void * new_thread(void* param){
+  int i;
+  struct Data *thread = param;
+  int chave = thread->chave;
+  for(int i = thread->pos_i;i < thread->pos_f;i++){
+    if(thread->v1[i] == chave){
+      return printf("Thread id: %ld, Posição inicio %d, Posição final: %d\n", pthread_self(),thread->pos_i, thread->pos_f);
+    }
+  }
+}
 
 int main(int argc, char *argv[])
 {
@@ -16,13 +41,13 @@ int main(int argc, char *argv[])
     rv* v1 = rv_gerar(tamVetor, 10000);
 
 
-    pthread_t *t1;
+    pthread_t t1[N];
    
 
   for(int i=0;i<N-1;i++){
-    Data *thread = criaData(i*(tamVetor/N),(i+1)*(tamVetor/N),v1);
+    Data *thread = criaData(i*(tamVetor/N),(i+1)*(tamVetor/N),v1, valorBuscar);
 
-    //pthread_create(&t1[i], NULL);
+    pthread_create(&t1[i], NULL, new_thread,  &v1->array[i]);
     pthread_join (t1[i], NULL);
   
   }
--- LAB3/ex1/thread.h
+++
@@ -1,9 +0,0 @@
-
-typedef struct Data {
-    int pos_i,pos_f;
-    int *v1;
-}Data;
-
-
-
-Data* criaData(int ini, int fim, int* v1);
